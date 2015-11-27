a732a72b3952986964e7b78899bc502e2bf050be
Piotr Łepkowski <ozrabal@gmail.com>
2015-11-27 14:28:01 +0100

    initial commit create and read places, display on map in frontend

---
+++ library/class-maptour.php
@@ -0,0 +1,148 @@
+<?php
+
+class Maptour {
+
+    private
+	    $plugin_name = 'maptour',
+	    $secret = 'RAJmIEgoEQATUufRPDov',
+	    $post_type = 'map_place';
+
+    public function __construct() {
+	if(is_admin()){
+	    $this->admin_setup();
+	}
+    }
+
+    public function admin_setup() {
+	add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+	add_action( 'init', array( $this, 'register_post_place' ) );
+	add_action( 'init', array( $this, 'register_taxonomy_place' ) );
+	add_action( 'add_meta_boxes', array( $this, 'add_meta_box') );
+	add_action( 'save_post', array( $this, 'save_meta_box' ) );
+    }
+
+    public function admin_enqueue_scripts() {
+	wp_enqueue_script( 'maps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&sensor=false' );
+	wp_enqueue_script( 'field-map', plugins_url( '../js/backend-map.js', __FILE__ ), array( 'jquery' ), MAPTOUR_VERSION );
+	wp_localize_script( 'field-map', 'geocode_notfound', __( 'No results were found for the search criteria', 'mt' ) );
+    }
+
+    public function register_post_place() {
+	$labels = array(
+	    'name' => __( 'Places', 'mt' ),
+	    'singular_name' => __( 'Place', 'mt' ),
+	    'add_new' => __( 'Add New', 'mt' ),
+	    'add_new_item' => __( 'Add New place', 'mt' ),
+	    'edit_item' => __( 'Edit place', 'mt' ),
+	    'new_item' => __( 'New place', 'mt' ),
+	    'view_item' => __( 'View place', 'mt' ),
+	    'search_items' => __( 'Search places', 'mt' ),
+	    'not_found' => __( 'No placess found', 'mt' ),
+	    'not_found_in_trash' => __( 'No places found in Trash', 'mt' ),
+	    'parent_item_colon' => __( 'Parent place:', 'mt' ),
+	    'menu_name' => __( 'Map places', 'mt' ),
+	);
+	$args = array(
+	    'labels' => $labels,
+	    'hierarchical' => false,
+	    'description' => __( 'places', 'mt' ),
+	    'supports' => array( 'title', 'editor','custom-fields' ),
+	    'public' => true,
+	    'show_ui' => true,
+	    'show_in_menu' => true,
+	    'menu_position' => 6,
+	    'show_in_nav_menus' => true,
+	    'publicly_queryable' => true,
+	    'exclude_from_search' => true,
+	    'has_archive' => false,
+	    'query_var' => true,
+	    'can_export' => true,
+	    'rewrite' => true,
+	    'capability_type' => 'post'
+	);
+	register_post_type( $this->post_type, $args );
+    }
+
+    public function register_taxonomy_place() {
+	$labels = array(
+		'name'                       => _x( 'Writers', 'taxonomy general name' ),
+		'singular_name'              => _x( 'Writer', 'taxonomy singular name' ),
+		'search_items'               => __( 'Search Writers' ),
+		'popular_items'              => __( 'Popular Writers' ),
+		'all_items'                  => __( 'All Writers' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Writer' ),
+		'update_item'                => __( 'Update Writer' ),
+		'add_new_item'               => __( 'Add New Writer' ),
+		'new_item_name'              => __( 'New Writer Name' ),
+		'separate_items_with_commas' => __( 'Separate writers with commas' ),
+		'add_or_remove_items'        => __( 'Add or remove writers' ),
+		'choose_from_most_used'      => __( 'Choose from the most used writers' ),
+		'not_found'                  => __( 'No writers found.' ),
+		'menu_name'                  => __( 'Writers' ),
+	);
+
+	$args = array(
+		'hierarchical'          => true,
+		'labels'                => $labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'place_type' ),
+	);
+
+	register_taxonomy( 'place_type', $this->post_type, $args );
+	
+    }
+
+
+    public function add_meta_box() {
+	add_meta_box( 'metabox_' . $this->plugin_name, __( 'Post place', 'mt' ), array( $this, 'render_metabox' ), $this->post_type );
+    }
+
+    public function render_metabox( $post ) {
+	wp_nonce_field( $this->secret . '_save', $this->plugin_name . '_nonce' );
+	$value = get_post_meta( $post->ID, 'place_data', true );
+	$type = 'hidden';
+	if ( MAPTOUR_DEBUG ) {
+	    $type = 'text';
+	}
+	$html = '<style>.map-box {height: 250px;}</style>'
+		. '<div id="field_map" class="map-field box">'
+		. '<input onkeydown="if (event.keyCode == 13){ codeAddress(); return false;}" id="geocode_field_map" class="controls" type="text" placeholder="' . __( 'Type location', 'mt' ) . '">'
+		. '<input type="button" class="code-address button button-small" value="' . __( 'Show on map', 'mt' ) . '" >'
+		. '<div id="map_field_map" class="map-box" ></div>'
+		. '<input id="map" class="geodata"  type="' . $type . '"  name="place_data" value="' . esc_attr( $value ) . '" />'
+		. '</div>';
+	echo $html;
+    }
+
+    function save_meta_box( $post_id ) {
+	if ( !filter_input( INPUT_POST, $this->plugin_name . '_nonce' ) ) {
+	    return;
+	}
+	if ( !wp_verify_nonce( filter_input( INPUT_POST, $this->plugin_name . '_nonce' ), $this->secret . '_save' ) ) {
+	    return;
+	}
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+	    return;
+	}
+	if ( filter_input( INPUT_POST, 'post_type' ) && 'page' == filter_input( INPUT_POST, 'post_type' ) ) {
+	    if ( ! current_user_can( 'edit_page', $post_id ) ) {
+		    return;
+	    }
+	} else {
+	    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		    return;
+	    }
+	}
+	if ( !filter_input( INPUT_POST, 'place_data' ) ) {
+	    return;
+	}
+	$my_data = sanitize_text_field( filter_input( INPUT_POST, 'place_data' ) );
+	update_post_meta( $post_id, 'place_data', $my_data );
+    }
+
+}
---
+++ map-tour.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * Plugin Name: Map Tour
+ * Plugin URI: http://
+ * Description:
+ * Version: 1.0.0
+ * Author:
+ * Author URI: http://webkowski.com
+ * Text Domain: mt
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'MAPTOUR_VERSION', '1.0.0');
+define( 'MAPTOUR_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'MAPTOUR_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'MAPTOUR_DEBUG', true );
+
+require_once 'library/class-maptour.php';
+
+add_action( 'plugins_loaded', function() {
+    new Maptour();
+});
+
+add_action( 'the_content', 'get_pages_map' );
+function get_pages_map( $content ) {
+    //dump(__FILE__);
+    $current_post_location = get_post_meta( get_the_ID(), 'place_data', true );
+    if( !$current_post_location ){
+	//return $content;
+    }
+    $current_post_location = '52.4064,16.9252, 15, roadmap';
+    $current_post_location = explode( ',', $current_post_location );
+    $map_args  = array(
+	'post_type'	=> 'map_place',
+	'post_status'	=> 'publish',
+	'meta_query' => array(
+	    array(
+		'key'	    => 'place_data',
+		'compare'   => 'EXIST',
+	    ),
+	),
+    );
+    $map_query = new WP_Query( $map_args );
+    if( $map_query->have_posts() ) {
+	while ( $map_query->have_posts() ) {
+	    $map_query->the_post();
+	    $page_location = explode( ',', get_post_meta( get_the_ID(), 'place_data', true ) );
+	    $page_location[] = '<div id="content"><h3><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h3>' . get_the_content() . '</div>';
+	    $markers[] = $page_location;
+	}
+    }
+    dump($markers);
+    ?>
+    <style>
+	.entry-content img, .comment-content img, .widget img {
+	    max-width: inherit;
+	}
+	#map {
+	    height: 300px;
+	    border: 1px solid #000;
+	}
+    </style>
+    <script>
+	var markers = <?php echo json_encode( $markers, JSON_HEX_QUOT | JSON_HEX_TAG ); ?>;
+    </script>
+    <div id="map"></div>
+    <script>
+	window.onload = function() {
+	    var latlng = new google.maps.LatLng(<?php echo trim($current_post_location[0]) ?>, <?php echo trim($current_post_location[1]) ?>);
+	    var map = new google.maps.Map(document.getElementById('map'), {
+		center: latlng,
+		zoom: <?php echo trim($current_post_location[2]) ?>,
+		mapTypeId: google.maps.MapTypeId.<?php echo strtoupper(trim($current_post_location[3])); ?>
+	    });
+	    var infowindow;
+	    for (var i = 0; i < markers.length; i++) {
+		var marker = new google.maps.Marker({
+		    position: new google.maps.LatLng(markers[i][0],markers[i][1] ),
+		    map: map,
+		    title: 'click to description',
+		    info: markers[i][4],
+		    icon: 'http://google.com/mapfiles/ms/micons/green-dot.png',
+		});
+		(function(marker, i) {
+		    google.maps.event.addListener(marker, 'click', function() {
+			if (infowindow) infowindow.close();
+			infowindow = new google.maps.InfoWindow({
+			    content: this.info
+			});
+			infowindow.open(map, marker);
+		    });
+		})(marker, i);
+	    }
+	};
+    </script>
+    <?php
+    wp_enqueue_script( 'maps', 'http://maps.google.com/maps/api/js?sensor=false', array(), true );
+    return $content;
+}
